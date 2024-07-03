489fc4bebd358113777dbdf0e88ed1960820304f
haoenlu <haoenlu.07@gmail.com>
2024-07-03 18:40:33 -0400

    update

--- main.py
+++ main.py
@@ -29,18 +29,16 @@ app.mount("/images",StaticFiles(directory=image_dir),name="images")
 
 with gr.Blocks().queue(default_concurrency_limit=10) as demo:
     
-    use_upload_image = gr.State(True)
+    image_url = gr.State("")
 
     with gr.Row() as row:
         with gr.Column() as col1:
 
             with gr.Tab("Upload Image") as tab1:
-                tab1.select(fn=lambda x: True,inputs=[use_upload_image],outputs=[use_upload_image])
                 input_image = gr.Image(label="Upload Image",type='filepath')
                 image_submit_button = gr.Button("Confirm")
 
             with gr.Tab("Upload URL") as tab2:
-                tab2.select(fn=lambda x: False,inputs=[use_upload_image],outputs=[use_upload_image])
                 input_image_url = gr.Text(label="Upload Image URL")
                 preview_image_box = gr.Image(visible=False,type='pil')
                 with gr.Row():
@@ -48,7 +46,7 @@ with gr.Blocks().queue(default_concurrency_limit=10) as demo:
                     image_url_submit_button = gr.Button("Confirm")
 
             with gr.Tab("Setting"):
-                image_prompt = gr.Text(label="Prompt",value="What kind of music better suits this picture?")
+                image_prompt = gr.Text(label="Prompt",value="Describe the music that better suits this picture.")
                 with gr.Row():
                     llava_num_token = gr.Slider(minimum=10,maximum=50,step=1,label="Prompt Length",value=30)
                     musicgen_num_token = gr.Slider(minimum=100,maximum=1000,step=10,label="Music Length",value=500)
@@ -64,18 +62,20 @@ with gr.Blocks().queue(default_concurrency_limit=10) as demo:
 
 
 
-    @image_submit_button.click(inputs=[input_image,image_prompt,llava_num_token,musicgen_num_token,music_genre],outputs=[output_text,audio,generate_new_music_button])
+    @image_submit_button.click(inputs=[input_image,image_prompt,llava_num_token,musicgen_num_token,music_genre],outputs=[image_url,output_text,audio,generate_new_music_button])
     def handle_image_upload(input_image,image_prompt,llava_num_token,musicgen_num_token,music_genre):
         if input_image is None:
-            gr.Error('Please upload image first!')
+            raise gr.Error('Please upload image first!')
 
         # Load and Save Image to static folder
         image_name = Path(input_image).name
         image = Image.open(input_image)
         image.save(os.path.join(image_dir,image_name))
+
         # Construct image url
-        url = f"http://{os.environ['EC2_URL']}:{os.environ['EC2_PORT']}/images/{image_name}"
-        return inference(url,image_prompt,llava_num_token,musicgen_num_token,music_genre)
+        image_url = f"http://{os.environ['EC2_URL']}:{os.environ['EC2_PORT']}/images/{image_name}"
+
+        return inference(image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre)
 
 
     @image_url_preview_button.click(inputs=[input_image_url],outputs=[preview_image_box])
@@ -83,25 +83,23 @@ with gr.Blocks().queue(default_concurrency_limit=10) as demo:
         image = Image.open(requests.get(url,stream=True).raw)
         return gr.Image(visible=True,value=image,label="Preview",container=True)
     
-    def inference(input_image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre):
-        llava_result = llava_inference(input_image_url,image_prompt,llava_num_token)
+    def inference(image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre):
+        llava_result = llava_inference(image_url,image_prompt,llava_num_token)
         musicgen_result = musicgen_inference(llava_result + music_genre,musicgen_num_token)
         generate_new_music_button = gr.Button("Generate New Song",visible=True)
-        return llava_result, (int(musicgen_result['sample_rate']), np.array(musicgen_result['audio'][0]).astype(np.float32)) , generate_new_music_button
+        return image_url,llava_result, (int(musicgen_result['sample_rate']), np.array(musicgen_result['audio'][0]).astype(np.float32)) , generate_new_music_button
 
-    @image_url_submit_button.click(inputs=[input_image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre],outputs=[output_text,audio,generate_new_music_button])
+    @image_url_submit_button.click(inputs=[input_image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre],outputs=[image_url,output_text,audio,generate_new_music_button])
     def handle_image_url(input_image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre):
         if input_image_url == "":
-            gr.Error("Please Enter the URL of the image!")
-
+            raise gr.Error("Please Enter the URL of the image!")
+        
         return inference(input_image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre)
 
-    @generate_new_music_button.click(inputs=[use_upload_image,input_image,input_image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre],outputs=[output_text,audio,generate_new_music_button])
-    def handle_generate_new_song(use_upload_image,input_image,input_image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre):
-        if use_upload_image:
-            return handle_image_upload(input_image,image_prompt,llava_num_token,musicgen_num_token,music_genre)
-        else:
-            return handle_image_url(input_image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre)
+    @generate_new_music_button.click(inputs=[image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre],outputs=[image_url,output_text,audio,generate_new_music_button])
+    def handle_generate_new_song(image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre):
+        return inference(image_url,image_prompt,llava_num_token,musicgen_num_token,music_genre)
+    
 
 
 def llava_inference(image_url,image_prompt,num_token):
